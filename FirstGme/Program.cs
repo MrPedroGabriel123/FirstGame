cad6d441fefa8a7f9254e09fe7c40dd06346b0af
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-03-02 22:36:41 +0000

    added menu!

--- FirstGme/Program.cs
+++ FirstGme/Program.cs
@@ -5,6 +5,10 @@
         static void main()
         {
             Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Wecolme to my first game :D");
+            Console.WriteLine("Start new game");
+            Console.WriteLine("Credits");
+            Console.WriteLine("Exit");
         }
     }
 }
