cf449eb46f964647eaeb749ecc6acc08c9052b10
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-03-02 23:02:14 +0000

    added everything on menu

--- FirstGme/Program.cs
+++ FirstGme/Program.cs
@@ -1,14 +1,56 @@
 ﻿namespace Game
 {
-    class game
+    internal static class game
     {
-        static void main()
+        public static void Main()
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Wecolme to my first game :D");
-            Console.WriteLine("Start new game");
-            Console.WriteLine("Credits");
-            Console.WriteLine("Exit");
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wecolme");
+                Console.WriteLine("Start new game");
+                Console.WriteLine("Credits");
+                Console.WriteLine("Exit");
+                var UserFirstChoice = Console.ReadLine();
+                
+
+                if (string.IsNullOrEmpty(UserFirstChoice))
+                {
+                    Console.WriteLine("Noob thats nothing mate...");
+                    Console.ReadLine();
+                    continue;
+
+                }
+                if (UserFirstChoice == string.Empty)
+                {
+                    Console.WriteLine("You need to choose something");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                if (UserFirstChoice == "Wecolme")
+                {
+                    Console.WriteLine("Choose one of the 3 options bellow");
+                    Console.ReadLine();
+                }
+                if (UserFirstChoice == "Start new game")
+                {
+                    Console.WriteLine("Your game will start soon...");
+                    Console.ReadLine();
+                }
+                if (UserFirstChoice == "Credits")
+                {
+                    Console.WriteLine("Made by MrPedroGabriel123");
+                    Console.WriteLine("MrAmos and Thalos helped my when I was stuck with any type of code");
+                    Console.ReadLine();
+                    continue;
+                }
+                if (UserFirstChoice == "Exit")
+                {
+                    System.Environment.Exit(0);
+                }
+            }
+            
         }
     }
 }
