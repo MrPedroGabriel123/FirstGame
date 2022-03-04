7b984ae89f4be7bcba467ebce85178341bd7bf0c
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-03-04 21:07:30 +0000

    switch If to Switch (userFirstChoice)

--- FirstGme/Program.cs
+++ FirstGme/Program.cs
@@ -12,51 +12,47 @@
                 Console.WriteLine("Credits");
                 Console.WriteLine("Exit");
                 var userFirstChoice = Console.ReadLine();
-                
 
-               if (userFirstChoice == string.Empty)
+
+                switch (userFirstChoice)
                 {
-                    Console.WriteLine("You need to choose something");
-                    Console.ReadLine();
-                    continue;
+                    case "Welcome":
+                        Console.WriteLine("Choose one of the 3 options bellow");
+                        break;
+                    case "Start new game":
+                        Console.WriteLine("Your game will start soon...");
+                        Console.ReadLine();
+
+                        Console.WriteLine("??? :Oh, you woke up, what is your name?");
+                        var name = Console.ReadLine();
+                        Console.WriteLine($"Jeff:Hi {name}, my name is Jeff and you are in our bunker right now");
+                        Console.ReadLine();
+                        Console.WriteLine($" {name}: What happend here? Why are we in a bunker?");
+                        Console.ReadLine();
+                        Console.WriteLine("Jeff: Russia is attacking us ");
+                        Console.ReadLine();
+                        Console.WriteLine($" {name}: But why? Why are they attacking us? That doesnt make any sense!");
+                        Console.ReadLine();
+                        Console.WriteLine(
+                            $"Jeff: We dont know yet, we are still finding more people to create a group agaisnt russia");
+                        break;
+                    case "Credits":
+                        Console.WriteLine("Made by MrPedroGabriel123");
+                        Console.WriteLine("MrAmos and Thalos helped my when I was stuck with any type of code");
+                        Console.ReadLine();
+                        break;
+                    case "Exit":
+                        System.Environment.Exit(0);
+                        break;
                 }
 
-                if (userFirstChoice == "Wecolme")
-                {
-                    Console.WriteLine("Choose one of the 3 options bellow");
-                    Console.ReadLine();
-                    continue;
-                }
-                if (userFirstChoice == "Start new game")
-                {
-                    Console.WriteLine("Your game will start soon...");
-                    Console.ReadLine();
 
-                    Console.WriteLine("??? :Oh, you woke up, what is your name?");
-                    var name = Console.ReadLine();
-                    Console.WriteLine($"Jeff:Hi {name}, my name is Jeff and you are in our bunker right now");
-                    Console.ReadLine();
-                    Console.WriteLine($" {name}: What happend here? Why are we in a bunker?");
-                    Console.ReadLine();
-                    Console.WriteLine("Jeff: Russia is attacking us ");
-                    Console.ReadLine();
-                    Console.WriteLine($" {name}: But why? Why are they attacking us? That doesnt make any sense!");
-                    Console.ReadLine();
-                    Console.WriteLine($"Jeff: We dont know yet, we are still finding more people to create a group agaisnt russia");
-                    return;
-                    
-                }
-                if (userFirstChoice == "Credits")
-                {
-                    Console.WriteLine("Made by MrPedroGabriel123");
-                    Console.WriteLine("MrAmos and Thalos helped my when I was stuck with any type of code");
-                    Console.ReadLine();
-                    continue;
-                }
-                if (userFirstChoice == "Exit")
-                {
-                    System.Environment.Exit(0);
-                }
+
+
+
+
+
+
             }
             
         }
